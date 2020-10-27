7b8a1d752129b6f00518f9375dba08754d2b16ca
Rohan Ghosh <rohangho@live.com>
2020-10-27 15:03:24 +0530

    Added the rounded corner for the bottom sheet

--- app/src/main/java/com/demosample/demophonexdownload/MainActivity.kt
+++ app/src/main/java/com/demosample/demophonexdownload/MainActivity.kt
@@ -11,19 +11,44 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webDisplayer: WebView
+    private var sheetBehavior: BottomSheetBehavior<*>? = null
+    private lateinit var layoutBottomSheet: ConstraintLayout
+    private lateinit var downloadTitle : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        layoutBottomSheet = findViewById(R.id.bcks)
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet)
+        downloadTitle = findViewById(R.id.textHeadings)
+        sheetBehavior!!.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(view: View, i: Int) {
+                if (BottomSheetBehavior.STATE_EXPANDED == i) {
+                    layoutBottomSheet.setBackgroundResource(R.drawable.boxy_shape)
+                }
+                if (BottomSheetBehavior.STATE_COLLAPSED == i) {
+
+                    downloadTitle.visibility = View.VISIBLE
+                    layoutBottomSheet.setBackgroundResource(R.drawable.rounder_shape)
+                }
+                if (BottomSheetBehavior.STATE_HIDDEN == i) {
+
+                }
+            }
+
+            override fun onSlide(view: View, v: Float) {}
+        })
 
         webDisplayer = findViewById(R.id.webview);
-        // webDisplayer.loadUrl("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")
         webDisplayer.loadUrl("https://www.instagram.com")
         webDisplayer.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(
@@ -31,14 +56,12 @@ class MainActivity : AppCompatActivity() {
                 request: WebResourceRequest
             ): Boolean {
                 view.loadUrl(request.url.toString())
-                Log.e("HIIII", "shouldOverrideUrlLoading: ${request.url}" )
                 return true
             }
 
             override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
                 Log.e("HIIII", "onPageStarted: $url")
-
 
             }
 
@@ -47,6 +70,10 @@ class MainActivity : AppCompatActivity() {
             override fun onLoadResource(view: WebView?, url: String?) {
                 super.onLoadResource(view, url)
                 Log.e("HIIII", "onLoadResource: $url" )
+                if(url!!.contains(".mp4",true))
+                {
+                    layoutBottomSheet.visibility =  View.VISIBLE
+                }
             }
         }
         webDisplayer.settings.loadsImagesAutomatically = true
