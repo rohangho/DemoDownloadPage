dade390b45fc7914c216783b17b23c214c8611b9
Rohan Ghosh <rohangho@live.com>
2020-10-28 11:57:04 +0530

    Added the kotlin to utility

---
+++ app/src/main/java/com/demosample/demophonexdownload/utility/Utility.kt
@@ -0,0 +1,26 @@
+package com.demosample.demophonexdownload.utility
+
+import android.graphics.Bitmap
+import android.media.MediaMetadataRetriever
+import android.os.Build
+import java.util.*
+
+object Utility {
+    @Throws(Throwable::class)
+    fun retriveVideoFrameFromVideo(videoPath: String?): Bitmap? {
+        var bitmap: Bitmap? = null
+        var mediaMetadataRetriever: MediaMetadataRetriever? = null
+        try {
+            mediaMetadataRetriever = MediaMetadataRetriever()
+            if (Build.VERSION.SDK_INT >= 14) mediaMetadataRetriever.setDataSource(videoPath, HashMap()) else mediaMetadataRetriever.setDataSource(videoPath)
+            //   mediaMetadataRetriever.setDataSource(videoPath);
+            bitmap = mediaMetadataRetriever.getFrameAtTime(1, MediaMetadataRetriever.OPTION_CLOSEST)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw Throwable("Exception in retriveVideoFrameFromVideo(String videoPath)" + e.message)
+        } finally {
+            mediaMetadataRetriever?.release()
+        }
+        return bitmap
+    }
+}
